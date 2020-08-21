7289d03d2bc85edf40cd408de92e8739a717bbe7
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2020-08-20 21:39:28 -0400

    Create MyContract1.sol

---
+++ MyContract1.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.6.0;
+
+contract MyContract {
+    string public myString = "Hello, world!";
+    bytes32 public myBytes32 = "Hello, world!";
+    int public myInt = 1;
+    uint public myUint = 1;
+    uint256 public myUint256 = 1;
+    uint8 public myUint8 = 1;
+    address public myAddress = 0x07bF4Cd2034129D8841d8c0b4444ffabC7882EA6;
+
+    struct MyStruct {
+        uint myUint;
+        string myString;
+    }
+
+    MyStruct public myStruct = MyStruct(1, "Hello, World!");
+
+    // Local variable
+    function getValue() public pure returns(uint) {
+        uint value = 1;
+        return value;
+    }
+}
