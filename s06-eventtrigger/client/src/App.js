458f18ca8724c51c1f7a7df6c297f06d3953c7f1
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2020-09-19 17:36:01 -0400

    front-end prototype completed
    unit testing next...

--- s06-eventtrigger/client/src/App.js
+++ s06-eventtrigger/client/src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
       );
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
+      this.listenToPaymentEvent();
       this.setState({loaded:true});
     
     } catch (error) {
@@ -39,6 +40,19 @@ class App extends Component {
     }
   };
 
+  listenToPaymentEvent = () => {
+    let self = this;
+    this.itemManager.events.SupplyChainStep().on("data", async function(evt) {
+      if(evt.returnValues._step == 1) {
+        let item = await self.itemManager.methods.items(evt.returnValues._itemIndex).call(
+        );
+        console.log(item);
+        alert("Item " + item._identifier + " was paid, deliver it now!");
+      };
+      console.log(evt);
+    });
+  }
+   
   handleSubmit = async() => {
     const { cost, itemName } = this.state;
     console.log(itemName, cost, this.itemManager);
