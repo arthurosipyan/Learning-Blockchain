8f8997d483a4c62e3902bb8730a193ca313ff2c8
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2020-09-23 22:18:00 -0400

    testing and tokensale implementation

--- s06-tokenization/migrations/2_deploy_contracts.js
+++ s06-tokenization/migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
-var MyToken = artifacts.require("./MyToken.sol");
+var MyToken = artifacts.require("./MyToken");
+var MyTokenSale = artifacts.require("./MyTokenSale")
 
 module.exports = async function(deployer) {
+    let addr = await web3.eth.getAccounts();
     await deployer.deploy(MyToken, 1000000000);
+    await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address);
+    let tokenInstance = await MyToken.deployed();
+    await tokenInstance.transfer(MyTokenSale.address, 1000000000);
 };
