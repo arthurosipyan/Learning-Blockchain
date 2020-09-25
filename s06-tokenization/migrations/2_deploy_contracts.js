6018bbeca4c1d8af2ed53ee9c7ab633ec47065f5
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2020-09-24 23:12:37 -0400

    kyc contract added with some new tests

--- s06-tokenization/migrations/2_deploy_contracts.js
+++ s06-tokenization/migrations/2_deploy_contracts.js
@@ -1,11 +1,13 @@
 var MyToken = artifacts.require("./MyToken");
 var MyTokenSale = artifacts.require("./MyTokenSale")
+var KycContract = artifacts.require("./KycContract");
 require("dotenv").config({path: "../.env"});
 
 module.exports = async function(deployer) {
     let addr = await web3.eth.getAccounts();
     await deployer.deploy(MyToken, process.env.INITIAL_TOKENS);
-    await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address);
+    await deployer.deploy(KycContract);
+    await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address, KycContract.address);
     let tokenInstance = await MyToken.deployed();
     await tokenInstance.transfer(MyTokenSale.address, process.env.INITIAL_TOKENS);
 };
