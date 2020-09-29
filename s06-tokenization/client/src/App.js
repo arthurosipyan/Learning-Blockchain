687acd198e9bb22ed45506d027d9ba01c822ecd2
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2020-09-29 00:29:14 -0400

    buyTokens with event listeners added to ui

--- s06-tokenization/client/src/App.js
+++ s06-tokenization/client/src/App.js
@@ -7,7 +7,7 @@ import getWeb3 from "./getWeb3";
 import "./App.css";
 
 class App extends Component {
-  state = { loaded:false, kycAddress: "0x123", tokenSaleAddress: "" };
+  state = { loaded:false, kycAddress: "0x123", tokenSaleAddress: "", userTokens: 0 };
 
   componentDidMount = async () => {
     try {
@@ -37,7 +37,8 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ loaded:true, tokenSaleAddress: this.myTokenSale._address });
+      this.listenToTokenTransfer();
+      this.setState({ loaded:true, tokenSaleAddress: this.myTokenSale._address }, this.updateUserTokens);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -46,7 +47,20 @@ class App extends Component {
       console.error(error);
     }
   };
+
+  handleBuyToken = async () => {
+    await this.myTokenSale.methods.buyTokens(this.accounts[0]).send({from: this.accounts[0], value: 1});
+  }
+
+  updateUserTokens = async() => {
+    let userTokens = await this.myToken.methods.balanceOf(this.accounts[0]).call();
+    this.setState({userTokens: userTokens});
+  }
   
+  listenToTokenTransfer = async() => {
+    this.myToken.events.Transfer({to: this.accounts[0]}).on("data", this.updateUserTokens);
+  }
+   
   handleInputChange = (event) => {
     const target = event.target;
     const value = target.type === "checkbox" ? target.checked : target.value;
@@ -75,6 +89,8 @@ class App extends Component {
         <button type="button" onClick={this.handleKycSubmit}>Add Address to Whitelist</button>
         <h2>Buy Cappucino-Tokens</h2>
         <p>Send Ether to this address: {this.state.tokenSaleAddress}</p>
+        <p>You have: {this.state.userTokens}</p>
+        <button type="button" onClick={this.handleBuyToken}>Buy more tokens</button>
       </div>
     );
   }
